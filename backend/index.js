9eaf16b5ca3d7272271a5a95e2b7c5dad69a31d9
subashinir05 <subashinir05@gmail.com>
2024-06-30 12:08:14 +0530

    Project Setup

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+import path from "path"
+
+
+
+import connectDB from './config/db.js'
+
+dotenv.config()
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 3000
+
+// ROUTES
+app.use("/api/v1/users", userRoutes)
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
